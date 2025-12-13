372afb5bc41431222b0f32ded33e66b5e8ecb26d
Marcus Boberg <38211830+marcusboberg@users.noreply.github.com>
2025-12-13 22:28:18 +0100

    minor update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
+const isStaticExport = process.env.NEXT_STATIC_EXPORT === 'true';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  ...(isStaticExport ? { output: 'export' } : {}),
   images: {
     unoptimized: true,
     remotePatterns: [
