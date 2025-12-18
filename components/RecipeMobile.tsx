fbc9e950e1d0b41f80ebdfd7a4a381457b44091e
Marcus Boberg <38211830+marcusboberg@users.noreply.github.com>
2025-12-18 19:27:22 +0100

    Move desktop edit icon to image corner

--- components/RecipeMobile.tsx
+++ components/RecipeMobile.tsx
@@ -274,17 +274,17 @@ export function RecipeMobile({ slug, initialRecipe }: Props) {
         <a href="#/" className="back-button desktop back-button--floating">
           <i className="fa-solid fa-arrow-left" aria-hidden="true" /> Tillbaka
         </a>
-        <a
-          href={`#/edit/${liveRecipe.slug}`}
-          className="recipe-edit-button recipe-edit-button--desktop"
-          aria-label="Redigera"
-          title="Redigera"
-        >
-          <i className="fa-solid fa-pen-to-square" aria-hidden="true" /> Redigera
-        </a>
         <div className="recipe-desktop-content">
           <div className="recipe-desktop-card">
             <div className="recipe-desktop-card__body recipe-desktop-card__ingredients">
+              <a
+                href={`#/edit/${liveRecipe.slug}`}
+                className="recipe-edit-button recipe-edit-button--fab"
+                aria-label="Redigera"
+                title="Redigera"
+              >
+                <i className="fa-solid fa-pen-to-square" aria-hidden="true" />
+              </a>
               <div className="recipe-desktop-toggle" role="tablist" aria-label="Visa innehåll">
                 <span className={`recipe-desktop-toggle__bg ${activeView === 'ingredients' ? 'is-left' : 'is-right'}`} aria-hidden="true">
                   <span
